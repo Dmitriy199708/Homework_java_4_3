b6cc8c99187b921626d6dc5715c927875e3d1403
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2023-10-03 21:00:52 +0300

    Editing Code

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
         int monthlyPayment;
 
-        monthlyPayment = service.calculate (1_000_000,12,9.99);
+        monthlyPayment = service.calculate(1_000_000, 12, 9.99);
         System.out.println("Ежемесячный платеж на 1 год = " + monthlyPayment);
 
-        monthlyPayment = service.calculate (1_000_000,24,9.99);
+        monthlyPayment = service.calculate(1_000_000, 24, 9.99);
         System.out.println("Ежемесячный платеж на 2 год = " + monthlyPayment);
 
-        monthlyPayment = service.calculate (1_000_000,36,9.99);
+        monthlyPayment = service.calculate(1_000_000, 36, 9.99);
         System.out.println("Ежемесячный платеж на 3 год = " + monthlyPayment);
 
     }
